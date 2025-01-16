319f8c5bcb2b8fdb8947ff60cf45d0ee9d0f27d2
Marco Minerva <marco.minerva@gmail.com>
2025-01-16 15:10:11 +0100

    Add UseVectorSearch extension method for AzureSqlDbContextOptionsBuilder (#10)
    Co-authored-by: Shay Rojansky <roji@roji.org>

--- EFCore.SqlServer.VectorSearch/Extensions/SqlServerVectorSearchDbContextOptionsBuilderExtensions.cs
+++ EFCore.SqlServer.VectorSearch/Extensions/SqlServerVectorSearchDbContextOptionsBuilderExtensions.cs
@@ -5,23 +5,39 @@ using Microsoft.EntityFrameworkCore.Infrastructure;
 namespace Microsoft.EntityFrameworkCore;
 
 /// <summary>
-///     Extension method for enabling Azure SQL vector search via <see cref="SqlServerDbContextOptionsBuilder" />.
+///     Extension method for enabling Azure SQL vector search via <see cref="AzureSqlDbContextOptionsBuilder" />.
 /// </summary>
 public static class SqlServerVectorSearchDbContextOptionsBuilderExtensions
 {
     /// <summary>
-    ///     Adds Azure SQL vector search functionality to Entity Framework.
+    ///     Adds Azure SQL vector search functionality to Entity Framework Core.
+    /// </summary>
+    /// <returns> The options builder so that further configuration can be chained. </returns>
+    public static AzureSqlDbContextOptionsBuilder UseVectorSearch(this AzureSqlDbContextOptionsBuilder optionsBuilder)
+    {
+        AddVectorSearchOptionsExtensions(optionsBuilder);
+
+        return optionsBuilder;
+    }
+
+    /// <summary>
+    ///     Adds SQL Server vector search functionality to Entity Framework Core.
     /// </summary>
     /// <returns> The options builder so that further configuration can be chained. </returns>
     public static SqlServerDbContextOptionsBuilder UseVectorSearch(this SqlServerDbContextOptionsBuilder optionsBuilder)
     {
-        var coreOptionsBuilder = ((IRelationalDbContextOptionsBuilderInfrastructure)optionsBuilder).OptionsBuilder;
+        AddVectorSearchOptionsExtensions(optionsBuilder);
+
+        return optionsBuilder;
+    }
+
+    private static void AddVectorSearchOptionsExtensions(this IRelationalDbContextOptionsBuilderInfrastructure optionsBuilder)
+    {
+        var coreOptionsBuilder = optionsBuilder.OptionsBuilder;
 
         var extension = coreOptionsBuilder.Options.FindExtension<SqlServerVectorSearchOptionsExtension>()
                         ?? new SqlServerVectorSearchOptionsExtension();
 
         ((IDbContextOptionsBuilderInfrastructure)coreOptionsBuilder).AddOrUpdateExtension(extension);
-
-        return optionsBuilder;
     }
 }
