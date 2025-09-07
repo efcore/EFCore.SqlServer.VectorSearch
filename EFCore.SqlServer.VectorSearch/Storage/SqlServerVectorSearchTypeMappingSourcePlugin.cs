251fa61650f1d55c9eff84c0d752c17b5fa8f39a
Shay Rojansky <roji@roji.org>
2025-09-07 10:54:00 +0200

    Make vector type comparison case-insensitive
    Closes #11

--- EFCore.SqlServer.VectorSearch/Storage/SqlServerVectorSearchTypeMappingSourcePlugin.cs
+++ EFCore.SqlServer.VectorSearch/Storage/SqlServerVectorSearchTypeMappingSourcePlugin.cs
@@ -6,7 +6,7 @@ namespace EFCore.SqlServer.VectorSearch.Storage.Internal;
 public class SqlServerVectorSearchTypeMappingSourcePlugin : IRelationalTypeMappingSourcePlugin
 {
     public RelationalTypeMapping? FindMapping(in RelationalTypeMappingInfo mappingInfo)
-        => mappingInfo.StoreTypeNameBase is "vector"
+        => string.Equals(mappingInfo.StoreTypeNameBase, "vector", StringComparison.OrdinalIgnoreCase)
             ? new SqlServerVectorTypeMapping(
                 mappingInfo.Size ?? throw new ArgumentException("'vector' store type must have a dimensions facet"))
             : null;
